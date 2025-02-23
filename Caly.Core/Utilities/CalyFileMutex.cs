011051ec00e757677221fa275e93a796875b8ec0
BobLd <38405645+BobLd@users.noreply.github.com>
2025-02-23 15:55:03 +0000

    Move away from named mutex due to issue on Linux/MacOS and use file based mutex (see https://github.com/dotnet/runtime/issues/110348)

---
+++ Caly.Core/Utilities/CalyFileMutex.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace Caly.Core.Utilities
+{
+    // Known issue with named System.Threading.Mutex on Linux/MacOS with AOT
+    // See https://github.com/dotnet/runtime/issues/110348
+    // https://www.martyndavis.com/?p=440
+
+    // Solution:
+    // https://mvolo.com/fun-with-file-locking/
+
+    /// <summary>
+    /// File mutex implementation to ensure cross-platform compatibility.
+    /// <para>
+    /// Known issue with named System.Threading.Mutex on Linux/MacOS with AOT.
+    /// See <see href="https://github.com/dotnet/runtime/issues/110348"/> and
+    /// <see href="https://www.martyndavis.com/?p=440"/>
+    /// </para>
+    /// </summary>
+    public sealed class CalyFileMutex
+    {
+        private static readonly string _lockFileName = Path.Combine(Path.GetTempPath(), "caly.lock");
+
+        private FileStream? _lockFile;
+
+        /// <summary>
+        /// Same signature as default System.Threading.Mutex.
+        /// </summary>
+        /// <param name="initiallyOwned">Not in use.</param>
+        /// <param name="name">Not in use.</param>
+        public CalyFileMutex(bool initiallyOwned, string? name)
+        {
+            // Same signature as default System.Threading.Mutex
+        }
+
+        public bool WaitOne(TimeSpan timeout, bool b)
+        {
+            // Same signature as default System.Threading.Mutex
+
+            try
+            {
+                // Force FileMode.CreateNew - if the file already exists, should throw (done for Linux)
+                _lockFile = new FileStream(_lockFileName, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.Delete);
+            }
+            catch (IOException)
+            {
+                // File already in use
+                return false;
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception);
+            }
+
+            return true;
+        }
+
+        public void ReleaseMutex()
+        {
+            File.Delete(_lockFileName);
+
+            if (_lockFile is null)
+            {
+                throw new NullReferenceException("Cannot release file mutex, because the file is null.");
+            }
+            
+            _lockFile.Dispose();
+            _lockFile = null;
+        }
+    }
+}
