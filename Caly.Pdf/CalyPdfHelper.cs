1d50e2d280e2560e98c02c0f504be316e873ec20
BobLd <38405645+BobLd@users.noreply.github.com>
2025-05-01 21:10:27 +0100

    Update CalyNNWordExtractor to filter out punctuation and separator chars from grouping

---
+++ Caly.Pdf/CalyPdfHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Caly.Pdf
+{
+    internal static class CalyPdfHelper
+    {
+        public static bool IsPunctuation(this ReadOnlySpan<char> text)
+        {
+            for (int i = 0; i < text.Length; ++i)
+            {
+                if (char.IsPunctuation(text[i]))
+                {
+                    return true;
+                }    
+            }
+
+            return false;
+        }
+
+        public static bool IsSeparator(this ReadOnlySpan<char> text)
+        {
+            for (int i = 0; i < text.Length; ++i)
+            {
+                if (char.IsSeparator(text[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
